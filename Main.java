b607274f79777a62346571e2f0e4c54614a661ec
Monsenator008 <74153011+Monsenator008@users.noreply.github.com>
2020-11-14 11:21:04 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,46 @@
-class Main {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        
+        do {
+           menuPrompt();
+           int selection = input.nextInt();
+        
+           Switch (selection){  //creates a loop statement based on the users input
+              
+              case 1 :   System.out.println("Let's begin!");
+                         break;
+              case 2 :   instructions();
+                         break;
+              case 0 :   System.out.println("Goodbye");
+                         break;
+              default:   System.out.println("Invalid input, please try again.");
+          
+           }while (selection != 0 || selection != 1);
+           
+            //This is where the bulk of the game will be played after choosing option 1
     }
+        
+    private static void menuPrompt(){  //creates a menu option for the user to choose from
+        
+        System.out.println("Welcome to the Oregon Trail for Beginners");
+	    System.out.println("1 .. Begin Journey");
+		System.out.println("2 .. Read Instructions");
+		System.out.println("0 .. exit");
+		System.out.print("Your choice: ");
+	}
+            
+    private static void instructions(){ //briefly explains the objective and the way to play the game.
+        System.out.println("This game begins with a small group of 10 individuals who are making their journey along the famous Oregon Trail.");
+        System.out.println("When you begin the journey, you start travelling towards your destination at 10 miles each time option 1 is chosen.");
+        System.out.println("If you would like to rest for the night, choose option 2, " + 
+                              "you have the possibility of gaining a group member. You cannot exceed 10 group members.");
+        System.out.println("If you choose option 3, you choose to end the game and give up your journey.");
+        System.out.println("There are many treacherous pitfalls and dangerous bandits along the trail that will take away group members as you progress on the trail.");
+        System.out.println("It is wise to rest when there are few group members remaining to strengthen your odds of success."); 
+    }
+
 } 
