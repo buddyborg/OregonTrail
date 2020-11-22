55036389ec1a7ac60648c4cbcdcaedf59e3e71ec
Cachou1313 <74160397+Cachou1313@users.noreply.github.com>
2020-11-21 18:07:40 -0700

    Update OptionThreeGiveUp.java

--- OptionThreeGiveUp.java
+++ OptionThreeGiveUp.java
@@ -1,4 +1,8 @@
-class OptionThreeGiveUp {
-    public static void main(String[] args) {
-    }
-} 
+class OptionThreeGiveUp{
+
+   public static void main(String[] args){
+      
+      System.out.println("You have decided the journey is far too taxing on your group to continue, time to pack it up and head back home");
+      break;
+   }
+}
