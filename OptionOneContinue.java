720fe9833787455c5c9465825a6a79732f2b9638
Paulina Cruz <buddyborg99@gmail.com>
2020-11-22 01:14:27 -0700

    fixed formatting

--- OptionOneContinue.java
+++ OptionOneContinue.java
@@ -4,16 +4,15 @@ import java.util.Random;
 
 class OptionOneContinue {
    public static boolean chance(int percentChance) {
-      Random random = new Random();
-      return random.nextInt(100) < percentChance;
+      return new Random().nextInt(100) < percentChance;
       
    }
    
     public static void pick(UserStats stats) {
       stats.increaseMilesJourneyed();
-      if (OptionOneContinue.chance(60)){
+      if (OptionOneContinue.chance(60))
          OptionOneContinue.runIntoBandits(stats);
-      }
+      
     } 
     public static void runIntoBandits(UserStats stats){
       System.out.println("You ran into bandits and lost a member!");
