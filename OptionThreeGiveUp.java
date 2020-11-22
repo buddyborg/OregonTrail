18e9aef751d0e7db0c3f222be12fcc024e96dac6
Paulina Cruz <buddyborg99@gmail.com>
2020-11-21 23:19:10 -0700

    added comment, removed break;

--- OptionThreeGiveUp.java
+++ OptionThreeGiveUp.java
@@ -1,8 +1,9 @@
+//Written by Reese Spaulding 
+
 class OptionThreeGiveUp{
 
    public static void main(String[] args){
       
       System.out.println("You have decided the journey is far too taxing on your group to continue, time to pack it up and head back home");
-      break;
    }
 }
