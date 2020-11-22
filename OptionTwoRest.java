6a96dd59bc95787f5a42495ebbeb946dae1acca9
AphoticGhost <64286549+AphoticGhost@users.noreply.github.com>
2020-11-21 21:44:52 -0700

    Update OptionTwoRest.java

--- OptionTwoRest.java
+++ OptionTwoRest.java
@@ -1,4 +1,37 @@
+import java.util.Random; 
 class OptionTwoRest {
-    public static void main(String[] args) {
+    //fields 
+   
+    
+    //constructors 
+ 
+     //methods
+     public static void optionTwo(UserStats stats){
+    //Rest one
+     int min = 0; 
+     int max = 100; 
+     int random_roll = (int)(Math.random() * (max - min + 1) + min); 
+    
+    
+    
+    if (random_roll > 50) {
+     
+      stats.increaseMembers(); 
+      System.out.println("You've chosen to rest for the day! You gain one member, but unfortunately you were attacked by bandits and one person was killed!"); 
+      OptionTwoRest.enemyAttack(stats); 
+    } 
+    //Rest two 
+    else if (random_roll < 50) {
+    
+     System.out.println("You've chosen to rest for the day! You gain one person and you were not attacked by bandits!"); 
+     stats.increaseMembers();
+     }
     }
-} 
+    
+    public static void enemyAttack(UserStats stats){
+      stats.decreaseMembers(); 
+    }
+   
+    
+  }
+ 
