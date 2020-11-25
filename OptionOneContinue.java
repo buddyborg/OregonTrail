736ae0c42f2d0297798f6c35f65c84db7a464a7b
Paulina Cruz <buddyborg99@gmail.com>
2020-11-25 15:23:39 -0700

    add comments

--- OptionOneContinue.java
+++ OptionOneContinue.java
@@ -1,21 +1,20 @@
 //Written by Paulina Cruz
 
-import java.util.Random;
+import java.util.Random; //import java.util.Random; 
 
 class OptionOneContinue {
    public static boolean chance(int percentChance) {
-      return new Random().nextInt(100) < percentChance;
-      
+      return new Random().nextInt(100) < percentChance; //returns random percent change
    }
    
     public static void pick(UserStats stats) {
-      stats.increaseMilesJourneyed();
-      if (OptionOneContinue.chance(60))
-         OptionOneContinue.runIntoBandits(stats);
-      
+      stats.increaseMilesJourneyed(); //calls increaseMilesJourneyed(); from UserStats.java
+      if (OptionOneContinue.chance(60)) //if chance is greater than the 60% chance do this
+         OptionOneContinue.runIntoBandits(stats); //calls runIntoBandits(stats);
     } 
+    
     public static void runIntoBandits(UserStats stats){
-      System.out.println("You ran into bandits and lost a member!");
-      stats.decreaseMembers();
+      System.out.println("You ran into bandits and lost a member!"); //prints out message for user
+      stats.decreaseMembers(); //calls decreaseMembers(); from UserStats.java
     }
 } 
