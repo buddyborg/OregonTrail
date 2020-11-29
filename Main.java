1ac78ab2782b1a9fd5f975826d4e30a55223b29e
David Monsen <74153011+Monsenator008@users.noreply.github.com>
2020-11-28 19:08:00 -0700

    Update Main.java
    see block comment for modificiations

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/*
+   Block comments;; 11/28/2020, modified selection so that if option1 or option2 are chosen 3 times 
+   in a row forces the user to chose either option 3 or the option they haven't chosen
+   Still need to modify some more to allow the user to continue playing the game after victory or defeat
+   Tried to add in while statement at the beginning of the game that is default Y and trying to have it
+   be modified at the end of the game to be able to input either Y or N
+*/
 import java.util.Scanner; //imports java.util.Scanner;
 
 public class Main {
@@ -8,8 +15,11 @@ public class Main {
         int[] stats = new int[2];
         UserStats newUser = new UserStats(stats);
         boolean exit = false;
-        
-        do {
+        int count1 = 0;
+        int count2 = 0;
+        String continue1 = "Y";
+     while (continue1 == "Y"){
+        do{
            menuPrompt();
            selection = input.nextInt();
 
@@ -41,20 +51,56 @@ public class Main {
                   
                   // Get selection
                   selection = input.nextInt();
-              
-                  switch (selection){
-                     case 1   : 
-                        OptionOneContinue.pick(newUser);
-                                 break;
-                     case 2   :
-                        OptionTwoRest.optionTwo(newUser);
-                                 break;
-                     case 3   :
-                        System.out.println("Goodbye");
-                        exit = true;
-                                 break;
-                     default  :  System.out.println("Enter a valid selection");
-                                 break;
+                  if (count1 < 3 && count2 < 3){
+                     switch (selection){
+                        case 1   :  OptionOneContinue.pick(newUser);
+                                    count1++;
+                                    break;
+                        case 2   :  OptionTwoRest.optionTwo(newUser);
+                                    count2++;
+                                    break;
+                        case 3   :  losingScreen();
+                                    exit = true;
+                                    break;
+                        default  :  System.out.println("Enter a valid selection");
+                                    break;
+                     }
+                  }
+                  if (count1 == 3){
+                     System.out.println("Enter what you would like to do from the two options"); //prints out prompt for user
+                     gameMenuPrompt2(); //calls gameMenuPrompt();
+                     System.out.println(); //prints out empty line for formatting
+                     selection = input.nextInt();
+                     count1 = 0;
+                     
+                     switch (selection){
+                        case 2   :  OptionTwoRest.optionTwo(newUser);
+                                    count2++;
+                                    break;
+                        case 3   :  losingScreen();
+                                    exit = true;
+                                    break;
+                        default  :  System.out.println("Enter a valid selection");
+                                    break;
+                     }
+                  }
+                  if (count2 == 3){
+                     System.out.println("Enter what you would like to do from the two options"); //prints out prompt for user
+                     gameMenuPrompt3(); //calls gameMenuPrompt();
+                     System.out.println(); //prints out empty line for formatting
+                     selection = input.nextInt();
+                     count2 = 0;
+                     
+                     switch (selection){
+                        case 1   :  OptionTwoRest.optionTwo(newUser);
+                                    count2++;
+                                    break;
+                        case 3   :  losingScreen();
+                                    exit = true;
+                                    break;
+                        default  :  System.out.println("Enter a valid selection");
+                                    break;
+                     }
                   }
                   
                   // TODO: check if game is ended
@@ -69,7 +115,10 @@ public class Main {
                   }
                   // If newUser.getMembers() <= 0 - user loses
                }
+               System.out.print("Do you wish to play again (Y/N)? ");
+               continue1 = input.nextLine();
            // }
+      }
       }
 
   
@@ -88,6 +137,18 @@ public class Main {
        System.out.println("3 .. give up");
        System.out.print("Your choice: ");
     }
+    
+    private static void gameMenuPrompt2(){ // written by David Monsen
+       System.out.println("2 .. rest for the night");
+       System.out.println("3 .. give up");
+       System.out.print("Your choice: ");
+    }
+    
+    private static void gameMenuPrompt3(){ // written by David Monsen
+       System.out.println("1 .. continue journey");
+       System.out.println("3 .. give up");
+       System.out.print("Your choice: ");
+    }
             
     private static void instructions(){ //briefly explains the objective and the way to play the game. Written by David Monsen
        System.out.println("This game begins with a small group of 10 individuals who are making their journey along the famous Oregon Trail.");
@@ -101,11 +162,13 @@ public class Main {
     }
     
     public static void victoryScreen() { //victory screen
-      System.out.println("Congratulations! You made it!"); 
+      System.out.println("Congratulations! You made it!");
+
     }
     
     public static void losingScreen() { //losing screen
       System.out.println("You have no members remaining!");
+
     }
 
 } 
