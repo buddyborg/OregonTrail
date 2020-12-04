767508c87a832abd8be5556ab7d36258fad329d8
David Monsen <74153011+Monsenator008@users.noreply.github.com>
2020-12-04 14:08:43 -0700

    Update OptionTwoRest.java
    modified class to include EnemyStats in class beginning on line 36, let me know if it looks good and I will replace the upper code.

--- OptionTwoRest.java
+++ OptionTwoRest.java
@@ -33,3 +33,36 @@ class OptionTwoRest {
     
  }
  
+// //Made by Shiana Venegas 
+// // Modified by David Monsen to include EnemyStats class and methods
+// 
+// import java.util.Random; 
+// public class OptionTwoRest {
+//  
+//      //methods
+//      public static void optionTwo(UserStats stats){
+//     //Rest one
+//      int min = 0; 
+//      int max = 100; 
+//      int random_roll = (int)(Math.random() * (max - min + 1) + min); 
+//     
+//     
+//     
+//      if (random_roll > 50) {
+//         System.out.println("You have found a stray traveller who is eager to join your group");
+//         stats.increaseMembers();
+//         stats.decreaseMembers();
+//         System.out.println("Unfortunately, that traveller was being hunted by a group of bandits and have killed him");
+//         EnemyStats.enemyAttack(stats); 
+//     } 
+//     //Rest two 
+//        else if (random_roll < 50) {
+//     
+//          System.out.println("You've chosen to rest for the day! You gain one person and you were not attacked by bandits." + 
+//                               " No one died of any diseases either, so that is another bonus.");
+//          stats.increaseMembers();
+//  }
+// }
+// }
+//     
+//  
