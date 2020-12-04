9897a1da6a1370ad700330ac59164cf2ea19243c
David Monsen <74153011+Monsenator008@users.noreply.github.com>
2020-12-04 14:07:45 -0700

    Update OptionOneContinue.java
    modified the code, begins on line 23, let me know what you think or if you want to modify it at all just let me know and I can modify it in jGrasp

--- OptionOneContinue.java
+++ OptionOneContinue.java
@@ -18,3 +18,37 @@ class OptionOneContinue {
       stats.decreaseMembers(); //calls decreaseMembers(); from UserStats.java
     }
 } 
+
+//   
+// //Written by Paulina Cruz
+// // Modified by David Monsen to include EnemyStats class and methods
+// 
+// import java.util.Random; //import java.util.Random; 
+// 
+// class OptionOneContinue {
+//    public static boolean chance(int percentChance) {
+//       return new Random().nextInt(100) < percentChance; //returns random percent change
+//    }
+//    
+//     public static void pick(UserStats stats) {
+//       stats.increaseMilesJourneyed(); //calls increaseMilesJourneyed(); from UserStats.java
+//       System.out.println("Your group has travelled 10 miles");
+//       if (OptionOneContinue.chance(30)){ //if chance is greater than the 60% chance do this
+//          EnemyStats.enemyAttack(stats); //calls runIntoBandits(stats)
+//       }
+//       else if (OptionOneContinue.chance(60)){
+//          stats.decreaseMembers();
+//          System.out.println("One of your group members perished by spontaneous combustion. You don't know what that means.");
+//          EnemyStats.enemyAttack(stats);
+//       }
+//       else if (OptionOneContinue.chance(90)){
+//          stats.increaseMembers();
+//          System.out.println("You saved a traveller from wandering off a cliff. You have gained one member");
+//       }
+//     } 
+//     
+//     public static void runIntoBandits(UserStats stats){
+//       System.out.println("You ran into bandits and lost a member!"); //prints out message for user
+//       stats.decreaseMembers(); //calls decreaseMembers(); from UserStats.java
+//     }
+// } 
