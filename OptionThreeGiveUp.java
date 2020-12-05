5acdd94da965ba607c0053f3a4c1f989ed4f762b
Paulina Cruz <buddyborg99@gmail.com>
2020-12-05 00:20:00 -0700

    move printlns from losingScreen to this method

--- OptionThreeGiveUp.java
+++ OptionThreeGiveUp.java
@@ -1,9 +1,20 @@
-//Written by Reese Spaulding 
+/********************************************************************************************************
+ * Written by     : Reese Spaulding                                                                      *
+ * Modified by    : Reese Spaulding & Paulina Cruz                                                       *
+ * Date created   : 11/03/2020                                                                           *
+ * Dates modified : 11/28/2020 & 12/04/2020                                                              *
+ * 12/04/2020     : Modifications by Reese Spaulding                                                     *
+                    - changed it so it no longer contains a Main method                                  *
+ * 12/04/2020     : Modifications by Paulina Cruz                                                        *
+                    - moved printlns written by David here so that it prints out when method is called   *
+ ********************************************************************************************************/
 
-public class OptionThreeGiveUp{
-
-   public void losingScreen(){
-     
-      System.out.println("You have decided the journey is far too taxing on your group to continue, time to pack it up and head back home");
-   }
+public class OptionThreeGiveUp {
+    public static void losingScreen() {
+        System.out.println("Unfortunately, all of your group members have perished,");
+        System.out.println("either by wandering off a cliff, dying from dysentery, or being killed by bandits.");
+        System.out.println("A few may have even been lost by spontaneous combustion, something that appears");
+        System.out.println("to affect those travelling along this pathway. Strange");
+        System.out.println("So comes to a close the unfortunate tragedy of a group lost to the world on their journey to a new land");
+    }
 }
